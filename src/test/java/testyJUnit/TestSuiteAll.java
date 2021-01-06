ec2140b30ab091bc1f4dd19e8bb24aad752a8a75
Bartosz Rudnik <rudnik49@gmail.com>
2021-01-06 13:47:30 +0100

    Zestaw wszystkich testów

--- src/test/java/testyJUnit/TestSuiteAll.java
+++ src/test/java/testyJUnit/TestSuiteAll.java
@@ -1,12 +1,10 @@
 package testyJUnit;
 
-import obsluga.TestEntity;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @Suite.SuiteClasses({AplikacjaPracownikTest.class, FabrykaTest.class, WydarzenieTest.class, WyszukiwaczWydarzenTest.class})
 @RunWith(Categories.class)
-@Categories.ExcludeCategory(TestEntity.class)
-public class TestSuiteEntity {
+public class TestSuiteAll {
 }
