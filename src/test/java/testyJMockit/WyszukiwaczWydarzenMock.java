c4f173b12b30e7c029e405d9c9634558999da749
Bartosz Rudnik <rudnik49@gmail.com>
2021-01-06 13:36:51 +0100

    JMockit testy

--- src/test/java/testyJMockit/WyszukiwaczWydarzenMock.java
+++ src/test/java/testyJMockit/WyszukiwaczWydarzenMock.java
@@ -5,14 +5,23 @@ import mockit.Injectable;
 import mockit.Tested;
 import mockit.integration.junit4.JMockit;
 import obsluga.Wydarzenie;
+import obsluga.WydarzenieStacjonarne;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import prezentacja.WyszukiwaczWydarzen;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(JMockit.class)
 public class WyszukiwaczWydarzenMock {
+
+    private final Wydarzenie [] tmp = new Wydarzenie[]{new WydarzenieStacjonarne("Wroclaw","kurs tanca", "30.01.2021", "15:30", 60, 1, 25),
+            new WydarzenieStacjonarne("Warszawa","szycie na maszynie", "24.03.2021", "17:45", 45, 2, 10)};
 
     @Tested
     WyszukiwaczWydarzen wyszukiwacz;
@@ -22,6 +31,17 @@ public class WyszukiwaczWydarzenMock {
 
     @Injectable
     String dataWydarzenia = "30.01.2021";
+
+    @Injectable
+    String miejsceWydarzenia = "Wroclaw";
+
+    @Injectable
+    List<Wydarzenie> wydarzenia = Arrays.asList(tmp);
+
+    @Before
+    public void setUp(){
+        wyszukiwacz = new WyszukiwaczWydarzen();
+    }
 
     @Test
     public void wyszukajWydarzenieDataWydarzeniaTest(){
@@ -35,4 +55,43 @@ public class WyszukiwaczWydarzenMock {
         assertTrue(wyszukiwacz.wyszukajWydarzenieDataWydarzenia(wydarzenie, dataWydarzenia));
 
     }
+
+    @Test
+    public void wyszukajWydarzenieMiejsceWydarzenia(){
+
+        new Expectations(){
+            {
+                wydarzenie.getMiejsce(); result = "Wroclaw";
+                wydarzenie.getTypWydarzenia(); result = "Stacjonarne";
+            }
+        };
+
+        assertTrue(wyszukiwacz.wyszukajWydarzenieMiejsceWydarzenia(wydarzenie, miejsceWydarzenia));
+
+    }
+
+    @Test
+    public void dwaAtrybuty(){
+
+        List<Wydarzenie> resultList =
+                wyszukiwacz.dwaAtrybuty(" ", " ", miejsceWydarzenia, dataWydarzenia);
+
+        for(Wydarzenie wyd : resultList){
+
+            assertEquals(wyd.getMiejsce(), miejsceWydarzenia);
+            assertEquals(wyd.getData(), dataWydarzenia);
+
+        }
+
+    }
+
+    @Test
+    public void wyszukajWydarzenie(){
+
+        for(Wydarzenie wyd : tmp){
+            assertTrue(wyszukiwacz.wyszukajWydarzenie(wyd));
+        }
+
+    }
+
 }
